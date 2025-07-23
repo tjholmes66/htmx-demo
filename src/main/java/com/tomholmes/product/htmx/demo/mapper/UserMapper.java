6560b022b91c16798ea9ae4216f0bb81d1153dc8
Thomas Joseph Holmes Jr. <tom@tomholmes.net>
2025-07-23 14:39:42 -0400

    Updated usermapper to remove password an security answers

--- src/main/java/com/tomholmes/product/htmx/demo/mapper/UserMapper.java
+++ src/main/java/com/tomholmes/product/htmx/demo/mapper/UserMapper.java
@@ -2,10 +2,12 @@ package com.tomholmes.product.htmx.demo.mapper;
 
 import com.tomholmes.product.htmx.demo.dto.UserDTO;
 import com.tomholmes.product.htmx.demo.model.UserEntity;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserEntity dtoToEntity(UserDTO source);
+    @BeanMapping(ignoreUnmappedSourceProperties = {"password", "securityAnswer1","securityAnswer2"})
     UserDTO entityToDTO(UserEntity destination);
 }
